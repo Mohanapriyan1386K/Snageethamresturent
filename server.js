91f48ebef011acc09823ecdee96445518d6d2688
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2026-06-26 23:35:38 +0530

    bug fixed

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", productRoutes);
